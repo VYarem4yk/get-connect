54eeb916e153b9846d381950424ce92fd8b6ead6
Vladislav Yaremchuk <VYarem4yk@gmail.com>
2020-05-14 00:17:56 +0300

    Change project structure

---
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./MyPosts.css";
+import Post from "../Post/Post";
+
+const MyPosts = () => {
+  return (
+    <div>
+      My Posts
+      <div>
+        <textarea className="textareaPost"></textarea>
+        <button>Добавить пост</button>
+      </div>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+};
+
+export default MyPosts;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Profile.css";
+import MyPosts from "./MyPosts/MyPosts";
 const Profile = () => {
   return (
     <main className="app-main">
@@ -7,16 +8,7 @@ const Profile = () => {
         <img src="https://thumbs.dreamstime.com/b/%D1%88%D0%B8%D1%80%D0%BE%D0%BA%D0%BE%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%82%D0%BD%D0%B0%D1%8F-%D0%BF%D0%B0%D0%BD%D0%BE%D1%80%D0%B0%D0%BC%D0%B0-%D0%B0%D0%BD-%D1%88%D0%B0%D1%84%D1%82%D0%B0-%D0%BF%D1%83%D1%81%D1%82%D1%8B%D0%BD%D0%B8-82115128.jpg"></img>
       </div>
       <div> avatar + information</div>
-      <div>
-        {" "}
-        My Posts
-        <div>New Post</div>
-        <ul>
-          <li>post-1</li>
-          <li>post-2</li>
-          <li>post-3</li>
-        </ul>
-      </div>
+      <MyPosts />
     </main>
   );
 };
